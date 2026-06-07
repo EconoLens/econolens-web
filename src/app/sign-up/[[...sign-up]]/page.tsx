7f5cded220ec05f0684b2ca07cd435cd58778eb8
Khagan-kp <khagankp@gmail.com>
2026-06-07 12:24:25 +0530

    Update page.tsx

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-      <main className="flex min-h-screen items-center justify-center p-6 bg-[#F5F0E8]">
-            <SignUp />
-                </main>
-                  );
-                  }
+    <main className="flex min-h-screen items-center justify-center p-6 bg-[#F5F0E8]">
+      <SignUp />
+    </main>
+  );
+}
