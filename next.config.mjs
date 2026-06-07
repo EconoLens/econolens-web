2efb6d78149f5a15460e08f9edc60d7bc981ca24
Khagan-kp <khagankp@gmail.com>
2026-06-07 21:57:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com https://js.clerk.io https://*.clerk.accounts.dev",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com https://js.clerk.io https://*.clerk.accounts.dev https://clerk.econolens.co.in",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://cdn.sanity.io https://img.clerk.com",
