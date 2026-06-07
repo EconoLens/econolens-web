1de73189b622f6b3b085279259b13b8a098b4d18
Khagan-kp <khagankp@gmail.com>
2026-06-07 22:24:24 +0530

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -4,21 +4,21 @@ import dynamic from 'next/dynamic'
 import config from '../../../../sanity.config'
 
 const Studio = dynamic(
-    () => import('sanity').then((mod) => mod.Studio),
+  () => import('sanity').then((mod) => mod.Studio),
   {
-        ssr: false,
-        loading: () => (
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'sans-serif', color: '#666' }}>
-                          Loading EconoLens Studio...
-                </div>div>
-              ),
+    ssr: false,
+    loading: () => (
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'sans-serif', color: '#666' }}>
+        Loading EconoLens Studio...
+      </div>
+    ),
   }
-  )
+)
 
 export default function StudioPage() {
-    return (
-          <div style={{ height: '100vh' }}>
-                  <Studio config={config} />
-          </div>div>
-        )
+  return (
+    <div style={{ height: '100vh' }}>
+      <Studio config={config} />
+    </div>
+  )
 }
